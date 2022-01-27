176b0c7f68419eef7c0bfcdf7bedb42af214260c
KoHanSol <abc35572003@naver.com>
2022-01-27 22:52:03 +0900

    2.2.2 회원 도메인 실행과 테스트 - MemberApp.java로 실행되는지 체크 - MemberServiceTest.java를 이용해서 테스트 실행
    2.2.3 주문과 할인 도메인 설계

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,6 +5,9 @@ package hello.core.member;
  */
 public class MemberServiceImpl implements MemberService{
 
+    //회원 도메인 설계의 문제점
+    //다른 저장소 변경 시 OCP 원칙 위반
+    //현재 의존관계가 인터페이스뿐만이 아니라 구현까지 모두 의존하는 문제가 있다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     public void join(Member member) {
