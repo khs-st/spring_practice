0f0bfa5625ece989b0d2d043c7122d7fdcb68e55
KoHanSol <abc35572003@naver.com>
2022-02-06 13:18:17 +0900

    2.4.6 @Configuration과 바이트코드 조작의 마법 - 스프링은 클래스의 바이트코드를 조작하는 라이브러리를 사용 -> 싱글톤 보장

--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -5,6 +5,7 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -29,5 +30,14 @@ public class ConfigurationSingletonTest {
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository3);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository3);
+    }
+
+    @Test
+    @DisplayName("AppConfig 클래스명 호출")
+    void configurationDeep(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
     }
 }
