75cf4a6511f00963701903f14ebdeb85a5f5cc5c
KoHanSol <abc35572003@naver.com>
2022-02-20 19:18:40 +0900

    2.8.3 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 문제점 - 프로토타입 빈이 새로 생성되기는 하지만, 싱글톤 빈과 함께 계속 유지되는 것이 문제다.

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -10,7 +11,9 @@ import javax.annotation.PreDestroy;
 
 public class SingletonWithPrototypeTest1 {
 
-    //스프링 컨테이너에 프로토타입 빈 직접 요청
+    //clientBean이 내부에 가지고 있는 프로토타입 빈은 이미 과거에 주입이 끝난 빈이다.
+    //주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성이 된 것이지,
+    //사용 할 때마다 새로 생성되는 것이 아니다
     @Test
     void prototypeFind(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototyeBean.class);
@@ -21,6 +24,37 @@ public class SingletonWithPrototypeTest1 {
         PrototyeBean prototyeBean2 = ac.getBean(PrototyeBean.class);
         prototyeBean2.addCount();
         Assertions.assertThat(prototyeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void singletonClientUsePrototype(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototyeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+
+        int count1 = clientBean1.logic();
+        System.out.println("clientBean1.logic = "+count1);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+
+        int count2 = clientBean2.logic();
+        System.out.println("clientBean2.logic = "+count2);
+        Assertions.assertThat(count2).isEqualTo(2);
+    }
+
+    @Scope("singleton")
+    static class ClientBean{
+        private final PrototyeBean prtotypeBean;
+
+        @Autowired
+        public ClientBean(PrototyeBean prototyeBean){
+            this.prtotypeBean=prototyeBean;
+        }
+
+        public int logic(){
+            prtotypeBean.addCount();
+            int count = prtotypeBean.getCount();
+            return count;
+        }
+
     }
 
     @Scope("prototype")
