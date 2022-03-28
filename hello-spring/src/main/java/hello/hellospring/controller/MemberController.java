0a6d3cd164019d7e5311ded80a8b99f3fe5ae4eb
khs <gustjd1209@naver.com>
2022-03-28 11:09:31 +0900

    1. MemberFrom 수정(getter, setter 어노테이션으로 수정) 2. MemeberController 수정(@RequiredArgsConstructor 선언)    - 어노테이션으로 인한 final 멤버인스턴스 자동 생성자 주입    - PostMapping 메서드 필드패턴을 이용하여 service단에 파라미터 던짐

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -10,7 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 
+//자 이부분 아주 좋은 부분입니다. 생성자를 통한 DI
+//여기서 개꿀팁 lombok을 사용해서 위의 @RequiredArgsConstructor 이걸 설정해준다면
+//알아서 생성자 주입을 해줍니다. 단 final이 붙은 멤버인스턴스에 대해서만.
+//그러므로 밑의 @Autowired 통산 생성자 주입방식을 코딩하지않아도 자동 주입됨.
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
 
@@ -35,10 +41,18 @@ public class MemberController {
     //회원가입
     @PostMapping("/members/new")
     public String create(MemberForm form){
-        Member member = new Member();
-        member.setName(form.getName());
-        member.setId(form.getId());
-        memberService.join(member);
+
+        // Memeber domain에 @Builder 어노테이션을 선언해주었기에
+        // 밑의 setter를 이용해서 데이터를 저장 하지 않고 빌드 패턴을 이용하여 저장해주면 개간지 ㅇㅈ?
+        memberService.join(Memeber.builder()
+                .id(form.getId())
+                .name(form.getName())
+                .build());
+
+    //    Member member = new Member();
+    //    member.setName(form.getName());
+    //    member.setId(form.getId());
+    //    memberService.join(member);
 
         return "redirect:/";
     }
