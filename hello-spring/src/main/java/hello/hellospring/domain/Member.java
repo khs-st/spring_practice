676ece1ab8028dddce007377c7d8ce998ba736dd
khs <gustjd1209@naver.com>
2022-03-28 10:25:03 +0900

    Fork 후 clone 해서 Pull Request까지 Test

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -4,30 +4,34 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 //JPA 엔티티 매핑
+@Getter
+// 기본 생성자 자동 추가
+// public Posts(){}와 같은 효과
+@NoArgsConstructor
 @Entity
 public class Member {
-    
-    //DB가 자동 생성 -> IDENTITY
+
+    // Entity에는 setter를 만들지 말라고함, 영적인 데이터객체이기 떄문에 set으로 데이터를 넣지말라고 하네연 껄껄껄
+
+   //DB가 자동 생성 -> IDENTITY
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
+    // 사용시 ex)
+    // repositoryMember.save(Member.builder()
+    //                             .id(id)
+    //                             .name(name)
+    //                             .build());
+    @Builder
+    public Member (Long id, String name){
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
